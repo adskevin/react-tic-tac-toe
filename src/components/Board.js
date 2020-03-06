8719366e054eb1f3ea9fdb160c0608129767d197
Kevin Souza <kevin-souza@procergs.rs.gov.br>
2020-03-06 15:40:13 -0300

    fix the winner moves highlight

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,10 +8,11 @@ export default class Board extends React.Component {
       if(this.props.player_move === i) {
         player_move = true
       } else if(Array.isArray(this.props.player_move)){
-        this.props.player_move.map((value) => function(){
+        this.props.player_move.map((value) => {
           if(value === i){
             player_move = true
           }
+          return player_move
         })
       }
   
